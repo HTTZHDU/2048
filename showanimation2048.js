b0b726c6b051aec17848d94af8f4e0dc2545db1a
HTTZHDU <httzhdu@sina.com>
2016-12-27 21:39:04 +0800

    1

---
+++ showanimation2048.js
@@ -0,0 +1,12 @@
+function showNumberWithAnimation(i,j,randNumber ){
+	var numberCell = $('#number-cell-'+i+'-'+j);
+	numberCell.css('background-color',getNubmerBackgroundColor(randNumber ));
+	numberCell.css('color',getNumberColor(randNumber ));
+	numberCell.text(randNumber );
+	numberCell.animate({
+		width:"100px",
+		height: "100px",
+		top: getPosTop(i,j),
+		left: getPosLeft(i,j)
+	},50);
+}
